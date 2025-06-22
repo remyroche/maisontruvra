6a8a90bb8f49df943276f8421487600e7935a3f7
hydror <remy.roche75@gmail.com>
2025-06-22 22:45:37 +0200

    Update extensions.py

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,7 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from flask_mail import Mail
-from celery import Celery
-from flask_caching import Cache
+from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 mail = Mail()
-celery = Celery(__name__, broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
-cache = Cache()
+cors = CORS()
+limiter = Limiter(key_func=get_remote_address)
+
