ac3efd6ddefb1dd8ef2ef366368ddc2743280422
hydror <remy.roche75@gmail.com>
2025-06-23 16:07:41 +0200

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_cors import CORS
 from flask_mail import Mail
@@ -10,12 +9,13 @@ from config import Config
 import logging
 from logging.handlers import RotatingFileHandler
 import os
+from argon2 import PasswordHasher
 
 from .middleware import RequestLoggingMiddleware
 
 db = SQLAlchemy()
 migrate = Migrate()
-bcrypt = Bcrypt()
+ph = PasswordHasher()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'info'
@@ -29,7 +29,6 @@ def create_app(config_class=Config):
     # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
-    bcrypt.init_app(app)
     login_manager.init_app(app)
     CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
     mail.init_app(app)
@@ -169,5 +168,4 @@ def create_app(config_class=Config):
     from .admin_api.session_routes import admin_session_bp
     app.register_blueprint(admin_session_bp, url_prefix='/api/admin/sessions')
 
-    return app
     return app
