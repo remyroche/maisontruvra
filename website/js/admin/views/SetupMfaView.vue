df52006c7ef1758fb2b663a6889c3f5a33eeb5b0
hydror <remy.roche75@gmail.com>
2025-06-24 14:40:45 +0200

    Create SetupMfaView.vue

---
+++ website/js/admin/views/SetupMfaView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/SetupMfaView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="space-y-6">
+      <header><h1 class="text-3xl font-bold text-gray-800">Setup Multi-Factor Authentication</h1></header>
+      <p class="text-gray-600">This page will contain the UI for an admin to set up or disable MFA on their own account.</p>
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
