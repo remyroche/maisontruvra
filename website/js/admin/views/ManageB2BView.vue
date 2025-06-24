e22cb0a9758566a2c7c8fd2826d5464d79c87508
hydror <remy.roche75@gmail.com>
2025-06-24 14:16:09 +0200

    Create ManageB2BView.vue

---
+++ website/js/admin/views/ManageB2BView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/ManageB2BView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="bg-white p-8 rounded-lg shadow-md">
+      <h1 class="text-3xl font-bold text-gray-800">Manage B2B Accounts</h1>
+      <!-- ... UI to approve B2B accounts ... -->
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
