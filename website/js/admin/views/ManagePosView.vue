197f908f0e0e5f7876b429df7c9feb9822866060
hydror <remy.roche75@gmail.com>
2025-06-24 14:39:28 +0200

    Create ManagePosView.vue

---
+++ website/js/admin/views/ManagePosView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/ManagePosView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="space-y-6">
+      <header><h1 class="text-3xl font-bold text-gray-800">Point of Sale (POS)</h1></header>
+       <p class="text-gray-600">This page will contain the Point of Sale interface for in-person transactions.</p>
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
