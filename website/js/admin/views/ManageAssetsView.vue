bf91bed53e088119544a12055baecc1e79575927
hydror <remy.roche75@gmail.com>
2025-06-24 14:17:14 +0200

    Create ManageAssetsView.vue

---
+++ website/js/admin/views/ManageAssetsView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/ManageAssetsView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="bg-white p-8 rounded-lg shadow-md">
+      <h1 class="text-3xl font-bold text-gray-800">Manage Assets</h1>
+      <!-- ... UI for asset uploads and management ... -->
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
