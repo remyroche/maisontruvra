247d679cc9cc573a7ca930e7cef1c4cc96b466ac
hydror <remy.roche75@gmail.com>
2025-06-24 14:39:42 +0200

    Create ManageQuotesView.vue

---
+++ website/js/admin/views/ManageQuotesView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/ManageQuotesView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="space-y-6">
+      <header><h1 class="text-3xl font-bold text-gray-800">Manage Quotes</h1></header>
+       <p class="text-gray-600">This page will display and allow management of B2B quote requests.</p>
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
