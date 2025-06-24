e8c99146bf0f9719e3effbbc7679cd3cdd30b65d
hydror <remy.roche75@gmail.com>
2025-06-24 14:16:21 +0200

    Create ManageLoyaltyView.vue

---
+++ website/js/admin/views/ManageLoyaltyView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/ManageLoyaltyView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="bg-white p-8 rounded-lg shadow-md">
+      <h1 class="text-3xl font-bold text-gray-800">Manage Loyalty Tiers</h1>
+      <!-- ... CRUD UI for loyalty tiers ... -->
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
