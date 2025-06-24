7e84c662fd97ac27367995efdb2f1ec2bfaabd31
hydror <remy.roche75@gmail.com>
2025-06-24 14:15:59 +0200

    Create ManageReviewsView.vue

---
+++ website/js/admin/views/ManageReviewsView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/ManageReviewsView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="bg-white p-8 rounded-lg shadow-md">
+      <h1 class="text-3xl font-bold text-gray-800">Manage Reviews</h1>
+      <!-- ... UI to approve/deny reviews ... -->
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
