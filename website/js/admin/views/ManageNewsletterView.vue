dd90357b2a48b3ebd1e6242dfc08fd14cc34bbed
hydror <remy.roche75@gmail.com>
2025-06-24 14:39:14 +0200

    Create ManageNewsletterView.vue

---
+++ website/js/admin/views/ManageNewsletterView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/ManageNewsletterView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="space-y-6">
+      <header><h1 class="text-3xl font-bold text-gray-800">Manage Newsletter</h1></header>
+       <p class="text-gray-600">This page will display newsletter subscribers and allow for sending out campaigns.</p>
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
