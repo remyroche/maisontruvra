93085028f89342f39d7a1236ab5113b1732d97a2
hydror <remy.roche75@gmail.com>
2025-06-24 14:17:03 +0200

    Create AdminProfileView.vue

---
+++ website/js/admin/views/AdminProfileView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/AdminProfileView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="bg-white p-8 rounded-lg shadow-md">
+      <h1 class="text-3xl font-bold text-gray-800">My Profile</h1>
+      <!-- ... Form to update admin profile and password ... -->
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
