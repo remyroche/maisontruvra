be31f694119d4cd11b8206d7ca3b83cc63ddc1f4
hydror <remy.roche75@gmail.com>
2025-06-24 14:15:48 +0200

    Create ManageBlogView.vue

---
+++ website/js/admin/views/ManageBlogView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/ManageBlogView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="bg-white p-8 rounded-lg shadow-md">
+      <h1 class="text-3xl font-bold text-gray-800">Manage Blog</h1>
+      <!-- ... UI for articles and blog categories ... -->
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
