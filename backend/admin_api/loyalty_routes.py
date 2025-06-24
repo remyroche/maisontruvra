196cc2b472295c60f802d5713e89a5396ecc9bd7
hydror <remy.roche75@gmail.com>
2025-06-24 15:07:41 +0200

    Update loyalty_routes.py

--- backend/admin_api/loyalty_routes.py
+++ backend/admin_api/loyalty_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, jsonify
 from backend.services.loyalty_service import LoyaltyService
 from backend.utils.sanitization import sanitize_input
-from backend.auth.permissions import permissions_required
+from ..utils.decorators import admin_required, staff_required, roles_required, permissions_required
+from ..utils.decorators import log_admin_action
 
 loyalty_management_bp = Blueprint('loyalty_management_bp', __name__, url_prefix='/admin/loyalty')
 
@@ -9,6 +10,9 @@ loyalty_management_bp = Blueprint('loyalty_management_bp', __name__, url_prefix=
 
 @loyalty_management_bp.route('/settings', methods=['GET'])
 @permissions_required('MANAGE_LOYALTY_PROGRAM')
+@log_admin_action
+@roles_required ('Admin', 'Manager')
+@admin_required
 def get_loyalty_settings():
     """
     Get the current settings for the loyalty program.
@@ -22,6 +26,9 @@ def get_loyalty_settings():
 
 @loyalty_management_bp.route('/settings', methods=['PUT'])
 @permissions_required('MANAGE_LOYALTY_PROGRAM')
+@log_admin_action
+@roles_required ('Admin', 'Manager')
+@admin_required
 def update_loyalty_settings():
     """
     Update the settings for the loyalty program.
@@ -46,6 +53,9 @@ def update_loyalty_settings():
 
 @loyalty_management_bp.route('/users/<int:user_id>/points', methods=['POST'])
 @permissions_required('MANAGE_LOYALTY_PROGRAM')
+@log_admin_action
+@roles_required ('Admin', 'Manager')
+@admin_required
 def adjust_user_points(user_id):
     """
     Manually add or remove loyalty points for a specific user.
