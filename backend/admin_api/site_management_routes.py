2cd60dd3ca951908972ed6023cc676b0cf790dd2
hydror <remy.roche75@gmail.com>
2025-06-24 15:28:15 +0200

    Update site_management_routes.py

--- backend/admin_api/site_management_routes.py
+++ backend/admin_api/site_management_routes.py
@@ -2,12 +2,17 @@ from flask import Blueprint, request, jsonify
 from backend.services.site_settings_service import SiteSettingsService # Assumed service
 from backend.utils.sanitization import sanitize_input
 from backend.auth.permissions import permissions_required
+from backend.auth.permissions import admin_required, staff_required, roles_required, permissions_required
+from ..utils.decorators import log_admin_action
 
 site_management_bp = Blueprint('site_management_bp', __name__, url_prefix='/admin/site-settings')
 
 # READ all site settings
 @site_management_bp.route('/', methods=['GET'])
 @permissions_required('MANAGE_SITE_SETTINGS')
+@log_admin_action
+@roles_required ('Admin', 'Dev')
+@admin_required
 def get_site_settings():
     """
     Retrieve all site settings.
@@ -22,6 +27,9 @@ def get_site_settings():
 # UPDATE site settings
 @site_management_bp.route('/', methods=['PUT'])
 @permissions_required('MANAGE_SITE_SETTINGS')
+@log_admin_action
+@roles_required ('Admin', 'Dev')
+@admin_required
 def update_site_settings():
     """
     Update one or more site settings.
