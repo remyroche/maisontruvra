7052a35d0eac253e463e3ad8c322aac37a32a645
hydror <remy.roche75@gmail.com>
2025-06-24 09:45:35 +0200

    Update B2BPortal.vue

--- website/js/vue/components/B2BPortal.vue
+++ website/js/vue/components/B2BPortal.vue
@@ -1,22 +1,36 @@
-// website/source/js/vue/components/B2BPortal.vue
 <template>
-    <div>
+  <div class="flex flex-col min-h-screen bg-gray-100">
+    <B2BHeader />
+
+    <main class="flex-grow container mx-auto p-4 sm:p-6 lg:p-8">
+      <div class="space-y-8">
         <DashboardStats />
         <LoyaltyStatus />
         <InvoicesTable />
-    </div>
+      </div>
+    </main>
+
+    <B2BFooter />
+  </div>
 </template>
 
 <script setup>
 import { defineAsyncComponent } from 'vue';
+import B2BHeader from './layout/B2BHeader.vue';
+import B2BFooter from './layout/B2BFooter.vue';
 
+// Asynchronously load child components for better performance
 const DashboardStats = defineAsyncComponent(() => 
-    import('./b2b/DashboardStats.vue')
+  import('./b2b/DashboardStats.vue')
 );
 const LoyaltyStatus = defineAsyncComponent(() => 
-    import('./b2b/LoyaltyStatus.vue')
+  import('./b2b/LoyaltyStatus.vue')
 );
 const InvoicesTable = defineAsyncComponent(() => 
-    import('./b2b/InvoicesTable.vue')
+  import('./b2b/InvoicesTable.vue')
 );
 </script>
+
+<style scoped>
+/* Scoped styles for the B2B portal layout can be added here */
+</style>
