7f9709795cff62a39f09adbf5415229313f94ff6
hydror <remy.roche75@gmail.com>
2025-06-24 14:39:57 +0200

    Create ManageRolesView.vue

---
+++ website/js/admin/views/ManageRolesView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/ManageRolesView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="space-y-6">
+      <header><h1 class="text-3xl font-bold text-gray-800">Manage Roles & Permissions</h1></header>
+      <p class="text-gray-600">This page will allow for creating, editing, and deleting user roles and assigning permissions to them.</p>
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
