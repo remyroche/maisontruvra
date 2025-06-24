21ae9d0fe9f17b09de2caed917ddb95b0cc11316
hydror <remy.roche75@gmail.com>
2025-06-24 14:40:09 +0200

    Create ManageSessionsView.vue

---
+++ website/js/admin/views/ManageSessionsView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/ManageSessionsView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="space-y-6">
+      <header><h1 class="text-3xl font-bold text-gray-800">Manage Active Sessions</h1></header>
+       <p class="text-gray-600">This page will display all active user and admin sessions and allow for forced termination.</p>
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
