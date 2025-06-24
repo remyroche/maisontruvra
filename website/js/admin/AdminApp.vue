2620067aefb1688fb4aeaf5557408f1d812ccc89
hydror <remy.roche75@gmail.com>
2025-06-24 14:23:00 +0200

    Update AdminApp.vue

--- website/js/admin/AdminApp.vue
+++ website/js/admin/AdminApp.vue
@@ -1,18 +1,15 @@
 <!--
  * FILENAME: website/js/admin/AdminApp.vue
  * DESCRIPTION: The root Vue component for the entire Admin Portal SPA.
- *
- * This component is the entry point for the admin application. It simply
- * renders the main router view, which in turn will render the AdminLayout
- * and the appropriate page component based on the current URL.
+ * UPDATED: Added the NotificationProvider to make notifications globally available.
 -->
 <template>
   <div id="admin-portal">
     <router-view></router-view>
+    <NotificationProvider />
   </div>
 </template>
 
 <script setup>
-// This is the root component.
-// Global setup, if any, could be performed here.
+import NotificationProvider from './components/NotificationProvider.vue';
 </script>
