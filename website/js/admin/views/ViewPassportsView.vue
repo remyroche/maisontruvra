dabf47d9b1088bab0a4169999961400051a5648a
hydror <remy.roche75@gmail.com>
2025-06-24 14:41:00 +0200

    Create ViewPassportsView.vue

---
+++ website/js/admin/views/ViewPassportsView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/ViewPassportsView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="space-y-6">
+      <header><h1 class="text-3xl font-bold text-gray-800">View Product Passports</h1></header>
+      <p class="text-gray-600">This page will display generated product passports.</p>
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
