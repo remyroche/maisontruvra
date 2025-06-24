8def26dcd614f9cb33d643e808c9cba01062a2e1
hydror <remy.roche75@gmail.com>
2025-06-24 13:29:05 +0200

    Delete website/js/pages directory

--- website/js/pages/politique-confidentialite.js
+++
@@ -1,7 +0,0 @@
-// website/source/js/pages/politique-confidentialite.js
-import '../i18n.js';
-
-document.addEventListener('DOMContentLoaded', async () => {
-    // Initialize i18next for the privacy policy page
-    await initI18next('politique-confidentialite');
-});
