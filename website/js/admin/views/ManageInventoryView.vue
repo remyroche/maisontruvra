103bdbc4e9d11cd30c1ea52d53620f9162e249fd
hydror <remy.roche75@gmail.com>
2025-06-24 14:38:59 +0200

    Create ManageInventoryView.vue

---
+++ website/js/admin/views/ManageInventoryView.vue
@@ -0,0 +1,14 @@
+<!--
+ * FILENAME: website/js/admin/views/ManageInventoryView.vue
+-->
+<template>
+  <AdminLayout>
+    <div class="space-y-6">
+      <header><h1 class="text-3xl font-bold text-gray-800">Manage Inventory</h1></header>
+       <p class="text-gray-600">This page will allow direct editing of stock levels for all products.</p>
+    </div>
+  </AdminLayout>
+</template>
+<script setup>
+import AdminLayout from '../components/AdminLayout.vue';
+</script>
