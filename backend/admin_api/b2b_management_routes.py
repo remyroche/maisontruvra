85c4b684628ebeb6d1ef361324e37095b8aa9f9c
hydror <remy.roche75@gmail.com>
2025-06-24 15:21:59 +0200

    Update b2b_management_routes.py

--- backend/admin_api/b2b_management_routes.py
+++ backend/admin_api/b2b_management_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from backend.services.b2b_service import B2BService  # Assumed service
 from backend.utils.sanitization import sanitize_input
-from ..utils.decorators import admin_required, staff_required, roles_required, permissions_required
+from backend.auth.permissions import admin_required, staff_required, roles_required, permissions_required
 from ..utils.decorators import log_admin_action
 from utils.sanitization import sanitize_input
 from services.user_service import UserService
