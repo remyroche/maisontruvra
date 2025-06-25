b6271878b59d041eacdc565e72c5132a1e320562
hydror <remy.roche75@gmail.com>
2025-06-25 13:03:00 +0200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -104,8 +104,9 @@ class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') # No default for production
     # Enforce secure cookies in production
     SESSION_COOKIE_SECURE = True
-    SESSION_COOKIE_SAMESITE = 'Strict'
-    REMEMBER_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = 'Lax'
+
 
 # Dictionary to map environment names to config classes
 config_by_name = dict(
