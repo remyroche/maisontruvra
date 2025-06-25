a54a69795588f5d8676ab24812b98c7755204471
hydror <remy.roche75@gmail.com>
2025-06-25 12:36:45 +0200

    Update ManageCategoriesView.vue

--- website/js/admin/views/ManageCategoriesView.vue
+++ website/js/admin/views/ManageCategoriesView.vue
@@ -4,9 +4,20 @@
     <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
       <div class="md:col-span-2">
          <BaseDataTable :headers="headers" :items="categoriesStore.categories">
+            <template #row="{ item, children }">
+                <tr :class="{ 'bg-red-50 text-gray-500 italic': item.is_deleted }">
+                    <td v-for="header in headers" :key="header.value" class="px-6 py-4 whitespace-nowrap text-sm">
+                        <slot :name="`item-${header.value}`" :item="item">{{ getNestedValue(item, header.value) }}</slot>
+                    </td>
+                </tr>
+            </template>
              <template #item-actions="{ item }">
-                 <button @click="openModal(item)" class="text-indigo-600 hover:text-indigo-900 mr-4">Edit</button>
-                 <button @click="deleteCategory(item.id)" class="text-red-600 hover:text-red-900">Delete</button>
+                <div class="flex items-center space-x-2">
+                    <button v-if="!item.is_deleted" @click="openModal(item)" class="text-indigo-600 hover:text-indigo-900 text-sm">Edit</button>
+                    <button v-if="!item.is_deleted" @click="deleteCategory(item.id, 'soft')" class="text-yellow-600 hover:text-yellow-900 text-sm">Soft Delete</button>
+                    <button v-if="item.is_deleted" @click="restoreCategory(item.id)" class="text-green-600 hover:text-green-900 text-sm">Restore</button>
+                    <button @click="deleteCategory(item.id, 'hard')" class="text-red-600 hover:text-red-900 text-sm">Hard Delete</button>
+                </div>
              </template>
         </BaseDataTable>
       </div>
@@ -37,7 +48,11 @@ const headers = [
     { text: 'Actions', value: 'actions' },
 ];
 
-onMounted(() => categoriesStore.fetchCategories());
+const getNestedValue = (item, path) => {
+    return path.split('.').reduce((acc, part) => acc && acc[part], item);
+}
+
+onMounted(() => categoriesStore.fetchCategories({ include_deleted: true }));
 
 const openModal = (category) => {
     isEditing.value = true;
@@ -56,10 +71,17 @@ const saveCategory = async (data) => {
     formKey.value++;
 };
 
-const deleteCategory = (id) => {
-    if(confirm('Are you sure? Deleting a category may affect products.')) {
-        categoriesStore.deleteCategory(id);
+const deleteCategory = (id, type) => {
+    const action = type === 'soft' ? 'soft-delete' : 'PERMANENTLY DELETE';
+    if(confirm(`Are you sure you want to ${action} this category? Deleting a category may affect products.`)) {
+        const deleteAction = type === 'soft' ? categoriesStore.softDeleteCategory : categoriesStore.hardDeleteCategory;
+        deleteAction(id);
     }
 };
 
+const restoreCategory = (id) => {
+    if(confirm('Are you sure you want to restore this category?')) {
+        categoriesStore.restoreCategory(id);
+    }
+}
 </script>
