3b6ed43687662af72b14d0a9324394bcc4933848
hydror <remy.roche75@gmail.com>
2025-06-27 14:07:59 +0200

    Update Header.vue

--- website/vue/components/layout/Header.vue
+++ website/vue/components/layout/Header.vue
@@ -33,6 +33,7 @@
               <span class="sr-only">Rechercher</span>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" /></svg>
           </button>
+          <LanguageSwitcher />
           <router-link to="/account" class="text-gray-500 hover:text-gray-700" aria-label="Mon compte">
           <!-- ... user icon svg ... -->
           </router-link>
@@ -68,6 +69,8 @@
 <script setup>
 import { ref } from 'vue';
 import '../../../js/vue/mini-cart-loader.js';
+import LanguageSwitcher from './LanguageSwitcher.vue'; // Import the new component
+import { useI18n } from 'vue-i18n';
 
 const isMobileMenuOpen = ref(false);
 </script>
