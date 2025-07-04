9b9669652c3053612f93e576c16d199b8f420f13
hydror <remy.roche75@gmail.com>
2025-07-04 12:04:02 +0200

    Update __init__.py

--- backend/admin_api/__init__.py
+++ backend/admin_api/__init__.py
@@ -4,4 +4,4 @@ from flask import Blueprint
 admin_api_bp = Blueprint('admin_api_bp', __name__)
 
 # Import routes to register them with the blueprint
-from . import user_management_routes, product_management_routes, order_routes, dashboard_routes, site_management_routes, auth_routes, audit_log_routes, b2b_management_routes, blog_routes, loyalty_routes, monitoring_routes, newsletter_routes, passport_routes, pos_routes, quote_routes, review_routes, session_routes
+from . import (  # noqa: E402 user_management_routes, product_management_routes, order_routes, dashboard_routes, site_management_routes, auth_routes, audit_log_routes, b2b_management_routes, blog_routes, loyalty_routes, monitoring_routes, newsletter_routes, passport_routes, pos_routes, quote_routes, review_routes, session_routes
